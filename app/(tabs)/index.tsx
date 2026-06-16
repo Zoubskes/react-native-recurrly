5bdb71b5e9bb4b73b82110f37d87d8195234fff3
Zoubair Khan <zoubairkhan@hotmail.com>
2026-06-17 00:21:44 +0200

    implement fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,13 @@ import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
- 
 const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
-        Ji Ji stinkt!
+        Zoubskes
       </Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
         <Text>Go to onboarding</Text>
